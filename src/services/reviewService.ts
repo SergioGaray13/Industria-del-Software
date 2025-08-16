3d161be953ee61c7c22ec3a02ffcb27cf82cdc08
SergioGaray13 <sergio.garay@unah.hn>
2025-08-16 01:02:27 -0600

    Update reviewService.ts

--- src/services/reviewService.ts
+++ src/services/reviewService.ts
@@ -15,7 +15,6 @@ export async function obtenerResenasPorLugar(lugarId: string) {
 
   if (error) throw error
 
-  // Formateamos para mostrar el nombre del usuario
   return data.map((rev: any) => ({
     ...rev,
     user_name: rev.users ? `${rev.users.first_name} ${rev.users.last_name}` : rev.user_id
