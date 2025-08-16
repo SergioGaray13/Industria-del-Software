7776e7fc536214cc7784059cb1b2ed8b1d0c146b
SergioGaray13 <sergio.garay@unah.hn>
2025-08-16 01:01:25 -0600

    Update email.ts

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -2,9 +2,9 @@ import emailjs from 'emailjs-com'
 
 export function enviarCorreoDeSoporte(name: string, mensaje: string) {
   return emailjs.send(
-    'service_bucu8vo',              // ID de servicio
-    'template_dyxexy8',             // ID de plantilla
-    { name, mensaje },              // variables para tu plantilla
-    'TgAyhcGi-BA2UFJXB'               // ⚠️ Reemplaza esto por tu PUBLIC KEY de EmailJS
+    'service_bucu8vo',            
+    'template_dyxexy8',            
+    { name, mensaje },             
+    'TgAyhcGi-BA2UFJXB'               
   )
 }
